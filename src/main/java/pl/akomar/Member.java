0091454283a046f9061879968ee6d2eaba5d717b
akomar <ak.komar@gmail.com>
2014-07-23 23:07:41 +0200

    Initial commit

---
+++ src/main/java/pl/akomar/Member.java
@@ -0,0 +1,9 @@
+package pl.akomar;
+
+import com.hazelcast.core.Hazelcast;
+
+public class Member {
+    public static void main(String[] args) {
+        Hazelcast.newHazelcastInstance();
+    }
+}
